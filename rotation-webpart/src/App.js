953c00a231384b7aa670491b42dc14a24a121a5b
mcantin <mcantin@live.com>
2017-02-12 17:48:36 -0600

    bxslider

--- rotation-webpart/src/App.js
+++ rotation-webpart/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +12,14 @@ class App extends Component {
          This rotation webpart is something that you can use ont he front page of any SharePoint page or site. It makes announcements, events, or any sort of content slick and easy on the eyes.
         </p>
           <div className="App-body">
-            <div id="slidercontainer"></div>          
+            <div id="slidercontainer">
+              <ul class="bxslider">
+                <li><img src="/images/pic1.jpg" /></li>
+                <li><img src="/images/pic2.jpg" /></li>
+                <li><img src="/images/pic3.jpg" /></li>
+                <li><img src="/images/pic4.jpg" /></li>
+              </ul>
+            </div>          
           </div>
       </div>
     );
