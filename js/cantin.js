59e4a38e5e8f52605e0c5c10379dd9b879d170f7
mcantin <mcantin@live.com>
2017-08-20 10:23:14 -0500

    carousel work

--- js/cantin.js
+++ js/cantin.js
@@ -5,18 +5,7 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         nav:true,
-        items: 2,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
+        items: 1
         });
 
     $("#heros").click(function(){
